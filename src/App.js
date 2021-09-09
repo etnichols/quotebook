7c686e2a7971a2edd539ce68ae0f8560bd3c55df
Evan Thomas Nichols <etnichols@evans-mbp.myfiosgateway.com>
2021-09-08 20:40:13 -0400

    add markdown support, add poem

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import ReactMarkdown from 'react-markdown'
 import { List } from 'immutable'
 
 import Control from './components/Control'
@@ -20,7 +21,7 @@ const App = (props) => {
 
   const handleNextButton = () => {
     // Wrap to beginning if user hits the end of the quote array.
-    const next = index === (shuffled.size -1) ? 0 : index + 1
+    const next = index === shuffled.size - 1 ? 0 : index + 1
     setIndex(next)
   }
 
@@ -36,19 +37,23 @@ const App = (props) => {
     setCurrentQuote(nextQuote)
   }, [index])
 
-  const {quote, author} = currentQuote
+  const { quote, author } = currentQuote
+
+  let quoteWithQuotes = '\"' + quote + '\"'
 
   return (
-    <div className="text-center content-center overflow-auto h-screen" style={{
-      backgroundColor: randomColor(),
-      transition: 'background-color 1s ease'
-    }}>
-      <h1 className="text-white text-2xl h-16 p-12 app-header">
-        Quotebook
-      </h1>
+    <div
+      className="text-center content-center overflow-auto h-screen"
+      style={{
+        backgroundColor: randomColor(),
+        transition: 'background-color 1s ease',
+      }}
+    >
+      <h1 className="text-white text-2xl h-16 p-12 app-header">Quotebook</h1>
       <div className="bg-white text-center text-base p-8 m-4 md:w-5/12 md:mx-auto rounded-md quote-modal">
-        <p className="my-4">"{quote}"</p>
-        <hr/>
+        <ReactMarkdown>{quoteWithQuotes}</ReactMarkdown>
+        <br />
+        <hr />
         <p className="my-4">- {author}</p>
       </div>
       <div className="text-center p-12">
@@ -59,20 +64,22 @@ const App = (props) => {
           onClickFunction={handleBackButton}
         />
         <Control
-          width={"40"}
-          height={"40"}
+          width={'40'}
+          height={'40'}
           icon="arrow-forward"
           onClickFunction={handleNextButton}
         />
       </div>
     </div>
   )
-  }
+}
 
-function shuffle(arr){
-  let curIndex = arr.length, tempVal, randIndex
+function shuffle(arr) {
+  let curIndex = arr.length,
+    tempVal,
+    randIndex
 
-  while(0 !== curIndex){
+  while (0 !== curIndex) {
     // Pick remaining element.
     randIndex = Math.floor(Math.random() * curIndex)
     curIndex -= 1
@@ -86,4 +93,4 @@ function shuffle(arr){
   return arr
 }
 
-export default App;
+export default App
